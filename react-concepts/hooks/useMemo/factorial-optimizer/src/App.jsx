38c864c43c6cd5ac9b07dd5fd8fc343c37fd3409
Hajira Marwah <hajiranadia12@gmail.com>
2025-09-27 10:52:59 +0530

    added usememo hook

--- react-concepts/hooks/useMemo/factorial-optimizer/src/App.jsx
+++ react-concepts/hooks/useMemo/factorial-optimizer/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useState,useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function slowFactorial(n){
+  console.log("caculating factorial....")
+  if(n<=0)return 1
+  let result=1
+  for(let i=1;i<=n;i++){
+    //for heavy
+    for(let j=0;j<1e6;j++){}
+    result*=i
+  }
+  return result
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(5)
+  const[extra,setExtra]=useState(0)
+  // With useMemo - Factorial only recalculates when "number" changes
+ const fact=useMemo(()=>slowFactorial(number),[number])
+
+ 
+  // Without useMemo-runs Every render → factorial recalculates calling "caculating factorial...." everytime
+  //  const fact = slowFactorial(number);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Factorial Optimizer</h1>
+    <input type='number' value={number} onChange={(e)=>setNumber(parseInt(e.target.value))} />
+    <p>Number:{number}</p>
+    <p>Fcatorial:{fact}</p>
+    <button onClick={()=>setExtra(extra+1)}>Rendered (extra={extra})</button>
     </>
   )
 }
