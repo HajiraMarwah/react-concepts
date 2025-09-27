d98b75a7ea52081ab5c0d43c33c0660b34b40b69
Hajira Marwah <hajiranadia12@gmail.com>
2025-09-27 10:23:42 +0530

    added usecallback

--- react-concepts/hooks/useCallback/smart-counter/src/App.jsx
+++ react-concepts/hooks/useCallback/smart-counter/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useCallback, useState } from 'react'
 import './App.css'
 
+const Child=React.memo(({onClick})=>{
+  console.log("child is rendereed")  //without callback-calls everytime
+  return <button onClick={onClick}>Increment</button>
+});
 function App() {
+  const [text, setTect] = useState("")
   const [count, setCount] = useState(0)
+  
+//without useCallback new function gets created every render
+// const Increment=()=>{
+//   setCount(count+1) //child is rendereed  evrytime on incremnt
+// }
+
+
+//with callback function remain same untill dependcy changes
+const Increment=useCallback(()=>{
+   setCount((prev)=>prev+1)   //here with callback child rendered wont come
+},[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Count:{count}</h1>
+    <Child  onClick={Increment}/>
+    <input type='text' value={text} placeholder='type something' onChange={(e)=>setTect(e.target.value)} />
+    <p>Typed:{text}</p>
     </>
   )
 }
